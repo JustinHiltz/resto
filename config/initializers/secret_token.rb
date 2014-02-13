28e30b48b8468d5cface714307cf695db40e128c
Justin Hiltz <justin4770@gmail.com>
2014-02-13 12:57:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Resto::Application.config.secret_key_base = '28d6e53d21964860b9ba13e3535a896f0f2f281ddd32d82f6707be454445a4e3b517fe4c1d24aa1f05e05c26b48c1d4bb3cde2f70b7f38f18ac0b7c61951a518'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Resto::Application.config.session_store :cookie_store, key: '_resto_session'
